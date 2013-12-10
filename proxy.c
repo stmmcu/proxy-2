c2f40c2abbb1a6d2d783f3cd68d79797ef6f2421
nasane <nbossart@slu.edu>
2013-12-10 15:22:28 -0600

    about done

--- proxy.c
+++ proxy.c
@@ -6,22 +6,34 @@
  *   Nathan Bossart & Joe Mayer
  *   Dec 10, 2013
  */
+#include <iostream>
+#include <stdio.h>
+#include <errno.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netdb.h>
+#include <pthread.h>
+#include <string.h>
+#include <signal.h>
 
 #define BUFSIZE 4096
-#define FOURZEROFOUR "HTTP/1.1 404 Not Found\nContent-Length: 219\nContent-Type: text/html\n\n<html><head><title>404 Not Found</title></head><body><h1>404 Not Found</h1><img src='http://www.climateaccess.org/sites/default/files/Obi%20wan.jpeg'></img><p>These aren't the bytes you're looking for.</p></body></html>"
+#define FOURZEROFOUR "HTTP/1.1 404 Not Found\nContent-Length: 219\nContent-Type: text/html\n\n<html><head><title>404 Not Found</title></head><body><h1>404 Not Found</h1><img src='http://www.climateaccess.org/sites/default/files/Obi%20wan.jpeg'></img><p>These aren't the bytes you're looking for.</p></body></html>\0"
 
 // handle broken pipes
 void broken_pipe_handler(int signum) {}  // this function intentionally left blank
 
-// nice failure function
-void fail(const char* str) {perror(str); exit(1);}
+// nice failure functions
+void fail(const char* str)        {perror(str); exit(1);}
+void fail_thread(const char* str) {perror(str); pthread_exit(0);}
 
 // parse request for host
 char* parse_host(char* buf, char** saveptr) {
   char* tok;
   tok = strtok_r(buf, " \r\n", saveptr);
   while (tok != NULL) {
-    if (strcmp(ret, "Host:")==0) {
+    if (strcmp(tok, "Host:")==0) {
       tok = strtok_r(NULL, " \r\n", saveptr);
       return tok;
     }
@@ -30,46 +42,84 @@ char* parse_host(char* buf, char** saveptr) {
   return NULL;
 }
 
-// make sure connection closes
-void close_req(char* buf) {
-  // TODO: fill this in
+// clean up before terminating thread
+void close_connections(struct addrinfo* host, int sock, FILE* f) {
+  freeaddrinfo(host);
+  fclose(f);
+  printf("\e[1;34mConnection closed on fdesc %d\n\e[0m", sock);
+  close(sock);
+  pthread_exit(0);
 }
 
 // serve each connection
 void* serve_client(void* v) {
-  int sock = *(int*) v;
+  int sock_client = *(int*) v;
   free(v);
 
-  FILE* client_r = fdopen(sock, "r");
-  FILE* client_w = fdopen(sock, "w");
-  if (client_r==NULL || client_w==NULL) {
-    perror("fdopen");
-    pthread_exit(0);
-  }
+  // fdesc for client socket
+  FILE* client_r = fdopen(sock_client, "r");
+  if (client_r==NULL) fail_thread("fdopen");
   setlinebuf(client_r);
-  setlinebuf(client_w);
   
+  // obtain header
   char buf[BUFSIZE];
   char req[BUFSIZE];
   while (fgets(buf, BUFSIZE, client_r) != NULL) {
-    // TODO: check that req is big enough and resize otherwise!
     strcat(req, buf);
     if (strlen(buf) < 3) break;
   }
 
-  char* svptr1, svptr2;
-  char* host_name = parse_host(req, &svptr1);
-  char* port_nmbr = parse_port(req, &svptr2);
-  if (host_name==NULL || port_nmbr==NULL) pthread_exit(0);
-  close_req(req);
+  // find host and port
+  char* svptr;
+  char req_cpy[BUFSIZE];
+  strcpy(req_cpy, req);
+  char* host_name = parse_host(req_cpy, &svptr);
+  if (host_name==NULL) pthread_exit(0);
 
+  // dns stuff
   int err;
   struct addrinfo* host;
-  err = getaddrinfo(host_name, port_nmbr, NULL, &host);
+  err = getaddrinfo(host_name, "80", NULL, &host);
   if (err) {
-
-    // TODO: this is where you left off
-
+    fprintf(stderr, "%s : %s\n", host_name, gai_strerror(err));
+    int n = write(sock_client, FOURZEROFOUR, strlen(FOURZEROFOUR));
+    if (n<0) fail_thread("write");
+    close_connections(host, sock_client, client_r);
+  }
+  
+  // make server socket
+  int sock_server = socket(host->ai_family, SOCK_STREAM, 0);
+  if (sock_server < 0) fail_thread("socket");
+  
+  // connect to server socket
+  if (connect(sock_server, host->ai_addr, host->ai_addrlen)) {
+      
+    // we've failed, give a 404 and exit
+    int n = write(sock_client, FOURZEROFOUR, strlen(FOURZEROFOUR));
+    if (n<0) fail_thread("write");
+    close_connections(host, sock_client, client_r);
+    
+  } else {
+    
+    // write header
+    FILE* server_w = fdopen(sock_server, "w");
+    if (server_w == NULL) fail_thread("fdopen");
+    setlinebuf(server_w);
+    printf("----------\n%s----------\n", req);
+    fputs(req, server_w);
+    
+    // get response
+    int n = read(sock_server, buf, sizeof(buf));
+    while (n>0) {
+      n = write(sock_client, buf, n);
+      if (n<0) fail_thread("write");
+      n = read(sock_server, buf, sizeof(buf));
+    }
+    if (n<0) fail_thread("read");
+    fclose(server_w);
+  }
+  close(sock_server);
+  close_connections(host, sock_client, client_r);
 }
 
 int main(int argc, char* argv[]) {
@@ -97,7 +147,7 @@ int main(int argc, char* argv[]) {
   }
   int serv_sock = socket(host->ai_family, SOCK_STREAM, 0);
   if (serv_sock < 0) fail("socket");
-  if (bind(serv_socket, host->ai_addr, host->ai_addrlen)) fail("bind");
+  if (bind(serv_sock, host->ai_addr, host->ai_addrlen)) fail("bind");
   if (listen(serv_sock, 5)) fail("listen");
   freeaddrinfo(host);
 
